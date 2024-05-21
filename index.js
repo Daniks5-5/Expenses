796831cc53bd90e7b9b84f33f4c548361fa27143
Danil <you@gmail.com>
2024-05-21 18:46:53 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let expenses = []; //список трат
 const LIMIT = 20000;
+const valute = 'руб';
+const STATUS_GOOD = 'Всё хорошо';
+const STATUS_BAD = 'Всё плохо';
+
 const input = document.getElementById('js-input-expenses');
 const button = document.getElementById('js-input-btn');
 const historyList = document.getElementById('js-history__list');
@@ -7,11 +11,56 @@ const sumElement = document.getElementById('js-sum');
 const limit = document.getElementById('js-limit');
 const sumLimit =  document.getElementById('js-limit');
 const status = document.getElementById('js-status');
+const allSum = document.getElementById('js-limit-sum');
 
 limit.innerText = LIMIT;
 
-button.addEventListener('click', function(){
+init(expenses);
 
+button.addEventListener('click', function(){
+    //2 сохраняем значения 
+    inputValue(input);
+    renderStatus(expenses);
+    renderHistory(expenses);
+    renderSum(expenses);
+});
+
+function init(expenses){
+    sumElement.innerText = 0;
+    sumElement.innerText = calculateExpenses(expenses);
+};
+
+function calculateExpenses(expenses){
+     //подсчет суммы и вывод 
+     let sum = 0;
+     expenses.forEach(expense => {
+        sum += expense; //пробегаю по массиву
+       });
+       return sum;
+};
+
+function renderHistory(expenses) {
+     //3 вывод списка
+     let html = '';
+     expenses.forEach(expense => {
+       html  += `<li>${expense} ${valute}.</li>`; //задаем шаблон строки, выводим поочередно каждый новый эелемент
+     });
+     
+    historyList.innerHTML = `<ol>${html}</ol>`;
+};
+
+function renderSum(expenses){
+    sumElement.innerText = calculateExpenses(expenses)+`${valute}`;//выводим сумму
+}
+function renderStatus(expenses){  //5 вывод статуса
+    if(calculateExpenses(expenses)>LIMIT){
+        status.innerHTML = `<p class = "status-inner-bad"> ${STATUS_BAD} </p>`;
+    }
+    else{
+        status.innerHTML = `<p class = "status-inner"> ${STATUS_GOOD} </p>`;
+    }
+}
+function inputValue(input){
     //1 получение значения с input
     if(!input.value){ //проверка, если input пустой, о ничего не выводим в массив
         return;  
@@ -19,31 +68,5 @@ button.addEventListener('click', function(){
     //ParseInt конвертирует строки в целочисленное значение
     const expense = parseInt(input.value);//получаем что ввёл пользователь
     input.value = ''; //сброс значения в input после нажатия кнопки "Добавить"
-
-    //2 сохраняем значения 
     expenses.push(expense);//для добавления значений в массив
-
-    //3 вывод списка
-    let html = '';
-    expenses.forEach(expense => {
-      html  += `<li>${expense} руб.</li>`; //задаем шаблон строки, выводим поочередно каждый новый эелемент
-    });
-    historyList.innerHTML = `<ol>${html}</ol>`;
-
-    //4 подсчет суммы и вывод 
-    let sum = 0;
-    expenses.forEach(expense => {
-       sum += expense; //пробегаю по массиву
-      });
-      sumElement.innerText = `${sum} рублей`;//выводим сумму
-
-      //5 вывод статуса
-    if(sum>LIMIT){
-        status.innerHTML = `<p class = "status-inner-bad"> Всё плохо </p>`;
-    }
-    else{
-        status.innerHTML = `<p class = "status-inner">Всё хорошо </p>`;
-    }
-
-
-});
+}
